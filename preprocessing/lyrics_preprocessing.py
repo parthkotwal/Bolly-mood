43ee1a14dbba64cc466941d9cb99cd6412a0daf6
parthkotwal <parthkotwa07@gmail.com>
2025-01-19 02:39:27 -0800

    continuing to clean gurmukhi and other languages

--- preprocessing/lyrics_preprocessing.py
+++ preprocessing/lyrics_preprocessing.py
@@ -50,46 +50,71 @@ def process_romanized(lyrics: str):
     return lyrics
 
 
-def clean_lyrics(lyrics:str):
-    unwanted_patterns = [
-    r'\b(embed|you might also like|related songs|other songs you might like|advertisement|promo|Embed|1Embed|2Embed)\b',  # Exact match for common terms
-    r'\[.*?\]',  # Any text within square brackets
-    r'http[s]?://\S+',  # URLs 
-    r'--+',  # Long dashes or similar
-    r'^\s*$',  # Empty lines or lines containing only spaces
-    r"^\d+\s*Contributor.*?Lyrics", # n ContributorsSongName Lyrics
-    r'embed',
-    ]
+def clean_lyrics(lyrics: str, artist: str):
+    lyrics = lyrics.lower()
+    artist_parts = artist.split()
+    artist_first_name = artist_parts[0] if len(artist_parts) > 0 else ""
+    artist_last_name = artist_parts[1] if len(artist_parts) > 1 else ""
 
-    for pattern in unwanted_patterns:
+    patterns = [
+        r'\b(embed|you might also like|related songs|other songs you might like|advertisement|promo|Embed|1Embed|2Embed)\b',  # Exact match for common terms
+        r'\[.*?\]',  # Any text within square brackets
+        r'http[s]?://\S+',  # URLs
+        r'--+',  # Long dashes or similar
+        r'^\s*$',  # Empty lines or lines containing only spaces
+        r"^\d+\s*contributor.*?lyrics",  # n ContributorsSongName Lyrics
+        r'embed',
+        rf'see\s+{artist_first_name.lower()}\s*{artist_last_name.lower()}\s*liveget\s*tickets\s*as\s*low\s*as\s*\d+\s*you\s*might\s*also\s*like',
+        rf'see\s+{artist_first_name.lower()}\s*liveget\s*tickets\s*as\s*low\s*as\s*\+\s*you\s*might\s*also\s*like',
+        rf'see\s+{artist_last_name.lower()}\s*liveget\s*tickets\s*as\s*low\s*as\s*\d+\s*you\s*might\s*also\s*like',
+        rf'lyrics\s*source\s+{artist_first_name.lower()}\s*{artist_last_name.lower()}\s*liveget\s*tickets\s*as\s*low\s*as\s*\d+\s*you\s*might\s*also\s*like',
+        '\d+see\s+udit\s+narayan\s+liveget\s+tickets\s+as\s+low\s+as\s+\d+'
+    ]
+    for pattern in patterns:
         lyrics = re.sub(pattern, '', lyrics, flags=re.IGNORECASE)
-    lyrics = re.sub(r'\[.*?\]', '', lyrics)
+
     lyrics = re.sub(r'\s+', ' ', lyrics).strip()
     lyrics = re.sub(r'[^\w\s\u0A00-\u0A7F]', '', lyrics)
     lyrics = re.sub(r'[^\w\s\u0900-\u097F]', '', lyrics)
-    lyrics = lyrics.lower()
     return lyrics
 
-def clean_and_standardize(lyrics:str):
-    lyrics = clean_lyrics(lyrics)
-    script = detect_script(lyrics)
-    if script == 'devanagari':
-        lyrics = process_devanagari(lyrics)
-    elif script == 'gurmukhi':
-        lyrics = process_gurmukhi(lyrics)
-    elif script == 'tamil':
-        lyrics = process_tamil(lyrics)
-    elif script == 'bengali':
-        lyrics = process_bengali(lyrics)
-    else:
-        lyrics = process_romanized(lyrics)
-    return lyrics.lower()
+def clean_and_standardize(lyrics:str, artist:str, max_iterations: int = 5):
+    iterations = 0
+
+    while iterations < max_iterations:
+        lyrics = clean_lyrics(lyrics, artist)
+        script = detect_script(lyrics)
+        if script == 'roman':
+            break
+
+        if script == 'devanagari':
+            lyrics = process_devanagari(lyrics)
+        elif script == 'gurmukhi':
+            lyrics = process_gurmukhi(lyrics)
+        elif script == 'tamil':
+            lyrics = process_tamil(lyrics)
+        elif script == 'bengali':
+            lyrics = process_bengali(lyrics)
+
+        iterations += 1
+
+    lyrics = clean_lyrics(lyrics, artist).lower()
+    return lyrics
 
 
 df.columns = ['artist', 'title','raw_lyrics']
+
+row_index = 223  # Change this to the desired row number
+specific_entry = df.iloc[row_index]  # Access the specific row
+lyrics = specific_entry['raw_lyrics']  # Assuming the column is named 'lyrics'
+artist = specific_entry['artist']
+result = clean_and_standardize(lyrics, artist)
+print(result)
+
+
 df = df[df['artist'] != 'KK']
-df = df[df['title'] != 'Memu Aagamu']
-df['cleaned_lyrics'] = np.vectorize(clean_and_standardize)(df['raw_lyrics'])
+df = df[~df['title'].isin(['Memu Aagamu', 'Brosila', 'Mountain Dew'])]
+df['cleaned_lyrics'] = np.vectorize(clean_and_standardize)(df['raw_lyrics'], df['artist'])
 
 df = df[df['cleaned_lyrics'].str.len() >= 10]
 df['cleaned_lyrics'] = df['cleaned_lyrics'].str.replace('ऑ', 'au')
@@ -102,3 +127,5 @@ df = df[df['cleaned_lyrics'] != "song instrumental"]
 df.drop('raw_lyrics',inplace=True,axis=1)
 
 df.to_csv("data/lyrics_cleaned.csv",index=False)
+# df.to_clipboard()
+print("Dataset cleaned")
