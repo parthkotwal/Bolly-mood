395704697132677a03f58ddaece25b127ad0578d
Parth Kotwal <parthkotwa07@gmail.com>
2025-03-31 00:52:44 -0700

    dealing with missing songs

--- data/metadata/metadata_collect.py
+++ data/metadata/metadata_collect.py
@@ -1,43 +1,52 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from fuzzywuzzy import fuzz
+import rapidfuzz.fuzz as fuzz
+import pandas as pd
 import re
 from dotenv import load_dotenv
 import os
-from pathlib import Path
+from typing import List
+from tenacity import retry, stop_after_attempt, wait_exponential
+import requests.exceptions
 
-env_path = Path(__file__).resolve().parent / '.env'
-load_dotenv(dotenv_path=env_path)
+load_dotenv()
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
 client_manager = SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
-sp = spotipy.Spotify(client_credentials_manager=client_manager)
+sp = spotipy.Spotify(client_credentials_manager=client_manager, requests_timeout=20)
 
+@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=2, min=1, max=8))
 def search_song(genius_title:str, genius_artist:str):
-    query = f"track:{genius_title} artist:{genius_artist}"
-    results = sp.search(q=query, type="track", limit=5)
+    try:
+        query = f"track:{genius_title} artist:{genius_artist}"
+        results = sp.search(q=query, type="track", limit=5)
 
-    for track in results['tracks']['items']:
-        spotify_title = track["name"]
-        spotify_artists = [a["name"] for a in track["artists"]]
+        for track in results['tracks']['items']:
+            spotify_title = track["name"]
+            spotify_artists = [a["name"] for a in track["artists"]]
 
-        if match_artist(genius_artist, spotify_artists) and match_titles(genius_title, spotify_title):
-            return {
-            "spotify_id":track["id"],
-            "title":track["name"],
-            "artist":", ".join(spotify_artists),
-            "album": track["album"]["name"],
-            "release_date": track["album"]["release_date"],
-            "popularity": track["popularity"]
-            }
-
+            if match_artist(genius_artist, spotify_artists) and match_titles(genius_title, spotify_title):
+                print(f"'{track['name']}' by '{', '.join(spotify_artists)}' added to dataset.")
+                return {
+                    "spotify_id":track["id"],
+                    "title":genius_title,
+                    "artist":", ".join(spotify_artists),
+                    "album": track["album"]["name"],
+                    "release_date": track["album"]["release_date"],
+                    "popularity": track["popularity"]
+                }
+    
+    except requests.exceptions.ReadTimeout:
+        print(f"Timeout error for '{genius_title}' by '{genius_artist}'")
+        return None
+    
     return None
     
 def get_audio_features(spotify_id:str):
-    features = sp.audio_features([spotify_id])[0]
-
-    if features:
+    features = sp.audio_features([spotify_id])
+    if features and features[0]:
+        features = features[0]
         return {
             "acousticness":features["acousticness"],
             "danceability":features["danceability"],
@@ -53,24 +62,21 @@ def get_audio_features(spotify_id:str):
     
     return None
 
-def clean_title(title):
+def clean_title(title:str):
     title = re.sub(r"\(.*?\)|\[.*?\]|\".*?\"", "", title)
+    title = re.sub(r"[-–]?\s?(Lyrics|Lyric Video|Official Video|Song)$", "", title, flags=re.IGNORECASE)
+    title = re.sub(r"^From\s+", "", title, flags=re.IGNORECASE)
     title = re.sub(r'[\u0900-\u097F]+', '', title)
-    title = title.strip()
+    return title.strip().lower()
 
-    if " - " in title:
-        title = title.split(" - ")[-1].strip()
-
-    return title.lower()
-
-def match_titles(genius_title, spotify_title, threshold=80):
+def match_titles(genius_title:str, spotify_title:str, threshold=70):
     genius_title = clean_title(genius_title)
     spotify_title = clean_title(spotify_title)
 
-    similarity = fuzz.ratio(genius_title, spotify_title)
+    similarity = fuzz.partial_ratio(genius_title, spotify_title)
     return similarity >= threshold
 
-def match_artist(genius_artists, spotify_artists):
+def match_artist(genius_artist:str, spotify_artists:List[str]):
     genius_artist = genius_artist.lower()
     spotify_artists = [a.lower() for a in spotify_artists]
 
@@ -79,3 +85,21 @@ def match_artist(genius_artists, spotify_artists):
             return True
         
     return False
+
+lyrics_data = pd.read_csv("data/lyrics/lyrics_cleaned_labelled_gcp.csv")
+# spotify_data = lyrics_data.apply(lambda row: search_song(row['title'], row['artist']), axis=1)
+# spotify_data = spotify_data.dropna().apply(pd.Series)
+spotify_data = pd.read_csv("data/metadata/spotify_metadata.csv")
+
+missing_songs = lyrics_data[~lyrics_data['title'].isin(spotify_data['title'])]
+retrieved_metadata = missing_songs.apply(lambda row: search_song(row['title'], row['artist']), axis=1)
+
+spotify_data = pd.concat([spotify_data, retrieved_metadata.dropna()], ignore_index=True)
+
+print(f"After re-matching, total matched songs: {len(spotify_data)}")
+print(missing_songs)
+
+spotify_data.to_csv("data/metadata/spotify_metadata.csv", index=False)
+
+# missing_songs.to_csv("data/metadata/missing_songs.csv", index=False)
+print("Metadata extraction completed successfully!")
